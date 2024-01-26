097ce29d47f10811b3b6ecec97103b7906fc6f84
DiNeeZ <dneez86@gmail.com>
2024-01-26 15:32:10 +0200

    js on pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const fileInclude = require('gulp-file-include');
+const fileinclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
 const server = require('gulp-server-livereload');
 const clean = require('gulp-clean');
@@ -7,6 +7,8 @@ const fs = require('fs');
 const sourcemaps = require('gulp-sourcemaps');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const webpack = require('webpack-stream');
+const babel = require('gulp-babel');
 
 //Helper functions
 const plumberNotify = (title) => ({
@@ -26,17 +28,17 @@ gulp.task('clean', function (done) {
 });
 
 // HTML
-const fileIncludeSettings = fileInclude({
+const fileIncludeSettings = {
   prefix: '@@',
   basepath: '@file',
-});
+};
 
 gulp.task('html', function () {
   return gulp
     .src('./src/*.html')
     .pipe(plumber(plumberNotify('HTML')))
-    .pipe(fileIncludeSettings)
-    .pipe(gulp.dest('./dist/'));
+    .pipe(fileinclude(fileIncludeSettings))
+    .pipe(gulp.dest('./dist'));
 });
 
 // SASS
@@ -48,6 +50,16 @@ gulp.task('sass', function () {
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist/css'));
+});
+
+//JAVASCRIPT
+gulp.task('js', function () {
+  return gulp
+    .src('./src/js/*.js')
+    .pipe(plumber(plumberNotify('JS')))
+    .pipe(babel())
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('./dist/js'));
 });
 
 // IMAGES
@@ -75,9 +87,11 @@ gulp.task('server', function () {
   return gulp.src('./dist/').pipe(server(serverOptions));
 });
 
+//WATCH
 gulp.task('watch', function () {
+  gulp.watch('./src/**/*.html', gulp.parallel('html'));
   gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass'));
-  gulp.watch('./src/**/*.html', gulp.series('html'));
+  gulp.watch('./src/js/**/*.js', gulp.parallel('js'));
   gulp.watch('./src/img/**/*', gulp.parallel('images'));
   gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'));
   gulp.watch('./src/files/**/*', gulp.parallel('files'));
@@ -87,7 +101,7 @@ gulp.task(
   'default',
   gulp.series(
     'clean',
-    gulp.parallel('html', 'sass', 'images', 'fonts', 'files'),
+    gulp.parallel('html', 'sass', 'js', 'images', 'fonts', 'files'),
     gulp.parallel('server', 'watch')
   )
 );
