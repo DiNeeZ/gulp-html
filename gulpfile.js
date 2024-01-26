8b24db0d0ce7ba70670b29d04f0202b05320c010
DiNeeZ <dneez86@gmail.com>
2024-01-26 10:58:55 +0200

    tasks: html | sass | images | server

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,50 @@
 const gulp = require('gulp');
+const fileInclude = require('gulp-file-include');
+const sass = require('gulp-sass')(require('sass'));
+const server = require('gulp-server-livereload');
+const clean = require('gulp-clean');
+const fs = require('fs');
 
-gulp.task('hello', function (callback) {
-  console.log('Hello from gulp!');
-  callback();
+// CLEAN
+gulp.task('clean', function (done) {
+  if (fs.existsSync('./dist/')) {
+    return gulp.src('./dist/', { read: false }).pipe(clean());
+  }
+  done();
 });
 
-gulp.task('friend', function (callback) {
-  console.log('My Friend!');
-  callback();
+// INCLUDE FILES
+const fileIncludeSettings = fileInclude({
+  prefix: '@@',
+  basepath: '@file',
 });
 
-gulp.task('default', gulp.series('hello', 'friend'));
+gulp.task('html', function () {
+  return gulp
+    .src('./src/*.html')
+    .pipe(fileIncludeSettings)
+    .pipe(gulp.dest('./dist/'));
+});
+
+// SASS
+gulp.task('sass', function () {
+  return gulp
+    .src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+// COPY IMAGES
+gulp.task('images', function () {
+  return gulp.src('./src/img/**/*').pipe(gulp.dest('./dist/img/'));
+});
+
+// START SERVER
+const serverOptions = {
+  livereload: true,
+  open: true,
+};
+
+gulp.task('server', function () {
+  return gulp.src('./dist/').pipe(server(serverOptions));
+});
